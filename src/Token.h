50b33c5905e6626e76c18580246961bd2a7c9250
SuadoCowboy <luccacsg@gmail.com>
2025-02-02 13:38:33 -0300

    reference token type

--- src/Token.h
+++ src/Token.h
@@ -5,10 +5,23 @@
 #define SWEATCI_STATEMENT_SEPARATOR ';'
 #endif
 
+#ifndef SWEATCI_REFERENCE
+#define SWEATCI_REFERENCE '$'
+#endif
+
+#ifndef SWEATCI_REFERENCE_OPEN
+#define SWEATCI_REFERENCE_OPEN '{'
+#endif
+
+#ifndef SWEATCI_REFERENCE_CLOSE
+#define SWEATCI_REFERENCE_CLOSE '}'
+#endif
+
 namespace sci {
     enum TokenType : unsigned char {
         NONE = 0,
         IDENTIFIER,
+        REFERENCE, // '$' example: var "my var" "hello there"; echo "${my var}, how are you?". Yes it can use whitespace, but it's suggested not to
         STRING,
         EOS, // End Of Statement
         END
