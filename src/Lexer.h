d11c9114be6da40a6dc9372e96bb8e5f8adb9aa1
SuadoCowboy <luccacsg@gmail.com>
2025-02-02 13:19:00 -0300

    added some functions that will make sense soon

--- src/Lexer.h
+++ src/Lexer.h
@@ -7,9 +7,17 @@ namespace sci {
     public:
         const char* input = nullptr;
         size_t inputSize = 0;
+        size_t position = 0;
+        Token previousToken = NONE;
 
         Lexer(const char* input = nullptr, size_t inputSize = 0);
 
         Token advance();
+
+        /// @brief when an identifier is found
+        /// @return 
+        Token parseArguments();
+        Token parseQuotedString();
+        Token parseString();
     };
 }
