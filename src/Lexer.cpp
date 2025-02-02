b6983252ed38a049edfb47b30e99622cb8bed407
SuadoCowboy <luccacsg@gmail.com>
2025-02-02 12:35:08 -0300

    starting on lexer

---
+++ src/Lexer.cpp
@@ -0,0 +1,7 @@
+#include "Lexer.h"
+
+sci::Lexer::Lexer(const char* input, size_t inputSize) : input(input), inputSize(inputSize) {}
+
+sci::Token sci::Lexer::advance() {
+    
+}
--- src/Lexer.h
+++ src/Lexer.h
@@ -3,5 +3,13 @@
 #include "Token.h"
 
 namespace sci {
-    
+    class Lexer {
+    public:
+        const char* input = nullptr;
+        size_t inputSize = 0;
+
+        Lexer(const char* input = nullptr, size_t inputSize = 0);
+
+        Token advance();
+    };
 }
