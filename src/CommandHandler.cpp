08f46b85d431fca30a98afebe1b120a5b88701f2
SuadoCowboy <luccacsg@gmail.com>
2025-02-27 22:17:39 -0300

    create add function

--- src/CommandHandler.cpp
+++ src/CommandHandler.cpp
@@ -10,3 +10,12 @@ void sci::CommandHandler::call(SweatContext& ctx, Command& command) {
     ctx.pCommand = &command;
     command.callback(ctx);
 }
+
+bool sci::CommandHandler::add(const Command& command) {
+    if (commands.count(command.name) == 0)
+        return false;
+
+    commands[command.name] = command;
+
+    return true;
+}
--- src/CommandHandler.h
+++ src/CommandHandler.h
@@ -12,6 +12,14 @@ namespace sci {
         std::unordered_map<std::string_view, Command> commands;
 
         Command* get(const std::string_view& name);
+        /**
+         * @brief adds command to commands unordered_map
+         * 
+         * @param command name of the will be mapped in the commands variable
+         * @return true if command name is okay
+         * @return false if command with this name already exists
+         */
+        bool add(const Command& command);
 
         static void call(SweatContext& ctx, Command& command);
     };
