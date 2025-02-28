71e6544284e3a157ecb8109766279a0c8e7bf54f
SuadoCowboy <luccacsg@gmail.com>
2025-02-27 22:41:27 -0300

    add empty str to value in initializer with no value param

--- src/Token.cpp
+++ src/Token.cpp
@@ -1,7 +1,7 @@
 #include "Token.h"
 
 sci::Token::Token() {}
-sci::Token::Token(TokenType type) : type(type) {}
+sci::Token::Token(TokenType type) : type(type), value("") {}
 sci::Token::Token(TokenType type, const std::string& value) : type(type), value(value) {}
 
 uint8_t operator|(const sci::TokenType& l, const sci::TokenType& r) {
