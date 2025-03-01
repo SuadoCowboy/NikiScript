ddc8b253d79243ca478f76968c9934f28371e4fa
SuadoCowboy <luccacsg@gmail.com>
2025-03-01 12:39:16 -0300

    created handleCommandCall function

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -2,6 +2,25 @@
 
 #include "PrintCallback.h"
 
+void sci::handleCommandCall(SweatContext& ctx) {
+    if (ctx.pCommand->maxArgs == 1 && !ctx.arguments.arguments.empty()) {
+        std::string argument = "";
+        for (const auto& arg : ctx.arguments.arguments)
+            argument += arg+' ';
+
+        ctx.arguments.arguments = {argument};
+    }
+
+    if (ctx.pCommand->minArgs > ctx.arguments.arguments.size() || ctx.arguments.arguments.size() > ctx.pCommand->maxArgs) {
+        sci::printf(sci::PrintLevel::ERROR, "Expected arguments between [{}, {}] but received {}\n", ctx.pCommand->minArgs, ctx.pCommand->maxArgs, ctx.arguments.arguments.size());
+        ctx.pCommand->printUsage();
+        return;
+    }
+
+    ctx.pCommand->callback(ctx);
+    ctx.arguments.arguments.clear();
+}
+
 void sci::parse(SweatContext& ctx) {
     if (ctx.pLexer == nullptr)
         return;
@@ -11,7 +30,7 @@ void sci::parse(SweatContext& ctx) {
         switch (ctx.pLexer->token.type) {
         case TokenType::IDENTIFIER: { // can be either variable or command
             if (ctx.variables.count(ctx.pLexer->token.value) != 0) {
-                ctx.variables[ctx.pLexer->token.value];
+                ctx.variables[ctx.pLexer->token.value]; // TODO
                 break;
             }
 
@@ -20,45 +39,29 @@ void sci::parse(SweatContext& ctx) {
                 sci::printf(PrintLevel::ERROR, "Unknown identifier \"{}\"\n", ctx.pLexer->token.value);
                 ctx.pLexer->advanceUntil(static_cast<uint8_t>(TokenType::EOS));
             }
-
-            ctx.pLexer->advance();
-            ctx.arguments.arguments.clear();
-
-            if (ctx.pCommand->maxArgs > 1) {
-                while (!((TokenType::EOS|TokenType::END) & ctx.pLexer->token.type))
-                    ctx.arguments.arguments.push_back(ctx.pLexer->token.value);
-
-            } else if (ctx.pCommand->maxArgs == 1) {
-                std::string argument = "";
-
-                while (!((TokenType::EOS|TokenType::END) & ctx.pLexer->token.type)) {
-                    argument += ctx.pLexer->token.value+' ';
-                    ctx.pLexer->advance();
-                }
-                
-                if (!argument.empty())
-                    ctx.arguments.arguments = {argument};
-            }
-
-            if (ctx.pCommand->minArgs > ctx.arguments.arguments.size() || ctx.arguments.arguments.size() > ctx.pCommand->maxArgs) {
-                ctx.pCommand->printUsage();
-                break;
-            }
-
-            ctx.pCommand->callback(ctx);
+            break;
         }
 
         case TokenType::EOS:
+            handleCommandCall(ctx);
             break;
 
         case TokenType::NUMBER:
+            ctx.arguments.arguments.push_back(ctx.pLexer->token.value);
             break;
 
         case TokenType::STRING:
+            // TODO: if parameter should be number (or reference and string is not a reference) then print usage and explain
+            // TODO: if is reference, check if reference is number if parameter should be number
+            ctx.arguments.arguments.push_back(ctx.pLexer->token.value);
             break;
 
         default:
             break;
         }
+    
+        ctx.pLexer->advance();
     }
+
+    handleCommandCall(ctx);
 }
--- src/Parser.h
+++ src/Parser.h
@@ -3,5 +3,18 @@
 #include "SweatContext.h"
 
 namespace sci {
+    /**
+     * @brief is called in parse function when EOS or END is reached
+     * @note ctx.pCommand should point to a valid Command object
+     * @param ctx 
+     * @see sci::parse
+     */
+    void handleCommandCall(SweatContext& ctx);
+
+    /**
+     * @brief Handles commands and variables as well as their arguments
+     * 
+     * @param ctx 
+     */
     void parse(SweatContext& ctx);
 }
