cdf96ffe6786d90e6ee0f5b2a72997ff0cf965c1
SuadoCowboy <luccacsg@gmail.com>
2025-02-28 21:44:44 -0300

    implemented getFloat and getDouble code

--- src/SweatContext.cpp
+++ src/SweatContext.cpp
@@ -4,5 +4,22 @@ std::string& sci::Arguments::getString() {
     return arguments[offset++];
 }
 
-bool sci::Arguments::getFloat(SweatContext&, float&) {return 0;}
-bool sci::Arguments::getDouble(SweatContext&, double&) {return 0;}
+bool sci::Arguments::getFloat(SweatContext& ctx, float& output) {
+    try {
+        output = std::stof(arguments[offset++]);
+        return true;
+    } catch (...) {
+        sci::printf(sci::PrintLevel::ERROR, "\"{}\" is not a valid float. Argument #{}: {}\n", arguments[offset-1], offset-1, ctx.pCommand->maxArgs == 1? ctx.pCommand->argsDescriptions[0] : ctx.pCommand->argsDescriptions[offset-1]);
+        return false;
+    }
+}
+
+bool sci::Arguments::getDouble(SweatContext& ctx, double& output) {
+    try {
+        output = std::stod(arguments[offset++]);
+        return true;
+    } catch (...) {
+        sci::printf(sci::PrintLevel::ERROR, "\"{}\" is not a valid float. Argument #{}: {}\n", arguments[offset-1], offset-1, ctx.pCommand->maxArgs == 1? ctx.pCommand->argsDescriptions[0] : ctx.pCommand->argsDescriptions[offset-1]);
+        return false;
+    }
+}
