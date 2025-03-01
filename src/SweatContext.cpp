55ad76937e95e4b336c5322bab123ecc414e00f0
SuadoCowboy <luccacsg@gmail.com>
2025-03-01 19:09:05 -0300

    update how get functions work in Arguments

--- src/SweatContext.cpp
+++ src/SweatContext.cpp
@@ -4,22 +4,10 @@ std::string& sci::Arguments::getString() {
     return arguments[offset++];
 }
 
-bool sci::Arguments::getFloat(SweatContext& ctx, float& output) {
-    try {
-        output = std::stof(arguments[offset++]);
-        return true;
-    } catch (...) {
-        sci::printf(sci::PrintLevel::ERROR, "\"{}\" is not a valid float. Argument #{}: {}\n", arguments[offset-1], offset-1, ctx.pCommand->maxArgs == 1? ctx.pCommand->argsDescriptions[0] : ctx.pCommand->argsDescriptions[offset-1]);
-        return false;
-    }
+float sci::Arguments::getFloat() {
+    return std::stof(arguments[offset++]);
 }
 
-bool sci::Arguments::getDouble(SweatContext& ctx, double& output) {
-    try {
-        output = std::stod(arguments[offset++]);
-        return true;
-    } catch (...) {
-        sci::printf(sci::PrintLevel::ERROR, "\"{}\" is not a valid float. Argument #{}: {}\n", arguments[offset-1], offset-1, ctx.pCommand->maxArgs == 1? ctx.pCommand->argsDescriptions[0] : ctx.pCommand->argsDescriptions[offset-1]);
-        return false;
-    }
+double sci::Arguments::getDouble() {
+    return std::stod(arguments[offset++]);
 }
