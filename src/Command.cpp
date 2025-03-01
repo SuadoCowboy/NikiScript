1f049d87be6ed9f3c08e4058d844b3a68e59d9a6
SuadoCowboy <luccacsg@gmail.com>
2025-02-28 21:42:54 -0300

    add printUsage command

--- src/Command.cpp
+++ src/Command.cpp
@@ -1,8 +1,28 @@
 #include "Command.h"
 
+#include <sstream>
+
+#include "PrintCallback.h"
+
 sci::Command::Command() {}
 
 sci::Command::Command(const std::string_view& name, unsigned char minArgs, unsigned char maxArgs,
     CommandCallback callback, const std::string_view& usage, const std::vector<std::string_view>& argsDescriptions/*, void* pData*/)
   : name(name), minArgs(minArgs), maxArgs(maxArgs), callback(callback), usage(usage), argsDescriptions(argsDescriptions)/*,
     pData(pData)*/ {}
+
+void sci::Command::printUsage() {
+    std::stringstream oss;
+    bool isName = true;
+
+    for (uint64_t i = 0; i < argsDescriptions.size(); ++i) {
+        if (isName)
+            oss << argsDescriptions[i] << ": ";
+        else
+            oss << argsDescriptions[i] << ';';
+
+        isName = !isName;
+    }
+
+    sci::printf(PrintLevel::WARNING, "Usage: {}\nArguments: {}", usage, oss.str());
+}
--- src/Command.h
+++ src/Command.h
@@ -18,5 +18,7 @@ namespace sci {
 
         Command();
         Command(const std::string_view& name, unsigned char minArgs, unsigned char maxArgs, CommandCallback callback, const std::string_view& usage, const std::vector<std::string_view>& argsDescriptions/*, void* pData=nullptr*/);
+    
+        void printUsage();
     };
 }
