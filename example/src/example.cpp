43f71c1839d2ffd13916bc667dd1eca65fca04b8
SuadoCowboy <luccacsg@gmail.com>
2025-03-01 20:57:23 -0300

    testing printing variables

--- example/src/example.cpp
+++ example/src/example.cpp
@@ -103,7 +103,7 @@ int main(int, char**) {
 	//   return EXIT_FAILURE;
 	// }
 
-	std::string input = "test 123 penes; echo ${penes}; var pp \"HEY THEEERE\"; test 123 pp; echo ${pp}; test 123 abc";
+	std::string input = "penes; var pp \"HEY THEEERE\"; pp";
 	// for (int i = 1; i < argc; ++i)
 	//    input += std::string(argv[i]) + " ";
 
