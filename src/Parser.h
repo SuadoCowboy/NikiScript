197cc417a81370332fab6d53ec7d577da82dc788
SuadoCowboy <luccacsg@gmail.com>
2025-03-01 13:39:48 -0300

    minor doc change

--- src/Parser.h
+++ src/Parser.h
@@ -4,7 +4,7 @@
 
 namespace sci {
     /**
-     * @brief is called in parse function when EOS or END is reached
+     * @brief called in parse function when EOS or END is reached
      * @note ctx.pCommand should point to a valid Command object
      * @param ctx 
      * @see sci::parse
