eef38c7482cbd1574a109401782f5078f607830b
SuadoCowboy <luccacsg@gmail.com>
2025-03-01 18:15:30 -0300

    changed how ProgramVariable structure works

--- src/ProgramVariable.cpp
+++ src/ProgramVariable.cpp
@@ -1,16 +1,28 @@
 #include "ProgramVariable.h"
 
 #include "PrintCallback.h"
+#include "SweatContext.h"
 
 sci::ProgramVariable::ProgramVariable() {}
 
 sci::ProgramVariable::ProgramVariable(void* pVariable, const GetProgramVariableValue& get, const SetProgramVariableValue& set)
  : pValue(pVariable), get(get), set(set) {}
 
-std::string sci::getString(const ProgramVariable& var) {
-    return *static_cast<std::string*>(var.pValue);
+std::string sci::getString(ProgramVariable* pVar) {
+    return *static_cast<std::string*>(pVar->pValue);
 }
 
-void sci::setString(ProgramVariable& var, const std::string& str) {
-    *static_cast<std::string*>(var.pValue) = str;
+void sci::setString(ProgramVariable* pVar, const std::string& str) {
+    *static_cast<std::string*>(pVar->pValue) = str;
+}
+
+void sci::ProgramVariable::callback(SweatContext& ctx) {
+    ProgramVariable* pVar = static_cast<ProgramVariable*>(ctx.pData);
+    if (pVar == nullptr)
+        return;
+
+    if (ctx.arguments.arguments.size() == 1)
+        pVar->set(pVar, ctx.arguments.getString());
+    else
+        sci::printf(PrintLevel::ECHO, "Value: {}\n", pVar->get(pVar));
 }
--- src/ProgramVariable.h
+++ src/ProgramVariable.h
@@ -5,9 +5,11 @@
 
 namespace sci {
     struct ProgramVariable;
-
-    typedef std::string(*GetProgramVariableValue)(const ProgramVariable& var);
-    typedef void(*SetProgramVariableValue)(ProgramVariable& var, const std::string& str);
+    
+    typedef std::string(*GetProgramVariableValue)(ProgramVariable* pVar);
+    typedef void(*SetProgramVariableValue)(ProgramVariable* pVar, const std::string& str);
+    
+    struct SweatContext;
 
     struct ProgramVariable {
         void* pValue = nullptr;
@@ -15,12 +17,14 @@ namespace sci {
         GetProgramVariableValue get = nullptr;
         SetProgramVariableValue set = nullptr;
 
+        static void callback(SweatContext& ctx);
+
         ProgramVariable();
         ProgramVariable(void* pVariable, const GetProgramVariableValue& get, const SetProgramVariableValue& set);
     };
 
     typedef std::unordered_map<std::string, ProgramVariable> ProgramVariables;
 
-    std::string getString(const ProgramVariable& var);
-    void setString(ProgramVariable& var, const std::string& str);
+    std::string getString(ProgramVariable* pVar);
+    void setString(ProgramVariable* pVar, const std::string& str);
 }
