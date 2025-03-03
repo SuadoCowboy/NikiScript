0ed7afb92ef656fda33e17e88029cfd04617d990
SuadoCowboy <luccacsg@gmail.com>
2025-03-03 14:28:39 -0300

    uppercase first characters in docs

--- src/Parser.h
+++ src/Parser.h
@@ -4,40 +4,45 @@
 
 namespace sci {
 	/**
-	 * @brief clear anything related to the current lexer statement
-	 * @note it does not go to the next EOS
+	 * @brief Clear anything related to the current lexer statement
+	 * @note It does not go to the next EOS
 	 */
 	void clearStatementData(SweatContext& ctx);
 
 	/**
-	 * @brief called in parse function when EOS or END is reached
-	 * @note sets ctx.pCommand to nullptr
+	 * @brief Called in parse function when EOS or END is reached
+	 * @note Sets ctx.pCommand to nullptr
 	 * @note ctx.pCommand should point to a valid Command object
-	 * @param ctx 
+	 * @param ctx
 	 * @see sci::parse
 	 */
 	void handleCommandCall(SweatContext& ctx);
 
 	/**
-	 * @brief handles references in the string and checks if the parameter matches the type
-	 * 
+	 * @brief Handles references in the string and checks if the parameter matches the argument type
+	 * @note Possible argument types: s = string, i = integer, d = decimal, v = variable
 	 */
 	void handleArgumentToken(SweatContext& ctx);
 	
 	extern uint64_t maxConsoleVariableCalls; ///< 0 = unlimited
 
 	/**
-	 * @brief called in parse function when an ConsoleVariable is passed as a IDENTIFIER
+	 * @brief Called in parse function when an ConsoleVariable is passed as a IDENTIFIER
 	 * @note I find this algorithm very interesting and fun(it creates a bunch of lexers for each variable call instead of recursive calling)
 	 * @see sci::maxRecursiveCalls
-	 * @param ctx 
+	 * @param ctx
 	 */
 	void handleConsoleVariableCall(SweatContext& ctx);
 
 	/**
+	 * @brief if a loop variable is active, its script is ran here
+	 * @param ctx
+	 */
+	void updateLoopVariables(sci::SweatContext& ctx);
+
+	/**
 	 * @brief Parses and interpret scripts: handles commands and variables as well as their arguments
-	 * 
-	 * @param ctx 
+	 * @param ctx
 	 */
 	void parse(SweatContext& ctx);
 }
