68831e019c40ab0834e90421ee364a1fe39cc0f9
SuadoCowboy <luccacsg@gmail.com>
2025-03-03 11:14:31 -0300

    add toggle command

--- src/SweatCI.h
+++ src/SweatCI.h
@@ -7,25 +7,27 @@ namespace sci {
 	 * @brief shows command usage
 	 * @param command s[command?]
 	 */
-	void help_command(sci::SweatContext& ctx);
+	void help_command(SweatContext& ctx);
 
 	/**
 	 * @brief prints all the arguments passed
 	 * @param message s[message]
 	 */
-	void echo_command(sci::SweatContext& ctx);
+	void echo_command(SweatContext& ctx);
 
-	void var_command(sci::SweatContext& ctx);
-	void delvar_command(sci::SweatContext& ctx);
+	void var_command(SweatContext& ctx);
+	void delvar_command(SweatContext& ctx);
 
-    void math_command(sci::SweatContext& ctx);
+    void math_command(SweatContext& ctx);
+
+	void toggle_command(SweatContext& ctx);
 
 	/**
 	 * @brief adds default commands such as echo
 	 * @param ctx 
 	 * @see echo_command
 	 */
-	void registerCommands(sci::SweatContext& ctx);
+	void registerCommands(SweatContext& ctx);
 
-	void registerVariable(sci::SweatContext& ctx, const std::string& name, void* pVar, const GetProgramVariableValue& get, const SetProgramVariableValue& set);
+	void registerVariable(SweatContext& ctx, const std::string& name, void* pVar, const GetProgramVariableValue& get, const SetProgramVariableValue& set);
 }
