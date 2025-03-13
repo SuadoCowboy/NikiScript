13f0e99863ddd8f8dc98798eff38424dc81fc805
SuadoCowboy <luccacsg@gmail.com>
2025-03-13 12:23:49 -0300

    add docs for formatString

--- src/Utils.h
+++ src/Utils.h
@@ -5,13 +5,26 @@
 #include <sstream>
 
 namespace ns {
-	/// @warning this function is not meant to be used outside this header
-	template<typename ...>
+	/**
+	 * @warning this function is not meant to be used outside this header
+	 * @tparam  
+	 * @param format 
+	 * @param buf 
+	 */
+	 template<typename ...>
 	static void _formatStringValue(const std::string& format, std::stringstream& buf) {
 		buf << format;
 	}
 
-	/// @warning this function is not meant to be used outside this header
+	/**
+	 * @warning this function is not meant to be used outside this header
+	 * @tparam T 
+	 * @tparam Args 
+	 * @param format 
+	 * @param buf 
+	 * @param value 
+	 * @param args 
+	 */
 	template<typename T, typename ... Args>
 	static void _formatStringValue(const std::string& format, std::stringstream& buf, T value, Args& ... args) {
 		uint64_t idx = format.find("{}");
@@ -25,6 +38,13 @@ namespace ns {
 		_formatStringValue(format.substr(idx+2), buf, args...);
 	}
 
+	/**
+	 * @brief format string using {}. Example: `formatString("Hello, {}!", "World")` -> `"Hello, World!"`
+	 * @tparam Args 
+	 * @param format 
+	 * @param args 
+	 * @return std::string 
+	 */
 	template<typename ... Args>
 	std::string formatString(const std::string& format, Args ... args) {
 		std::stringstream buf;
@@ -34,36 +54,27 @@ namespace ns {
 
 	/**
 	  * @brief trim from start
-	  * 
 	  * @param s string to trim the beginning
-	  * 
 	  * @see ns::trim
 	  * @see ns::rtrim
-	  * 
 	  * @note https://stackoverflow.com/questions/216823/how-to-trim-a-stdstring
 	  */
 	void ltrim(std::string &s);
 	
 	/**
 	 * @brief trim from end
-	 * 
 	 * @param s string to trim the end
-	 * 
 	 * @see ns::trim
 	 * @see ns::ltrim
-	 * 
 	 * @note https://stackoverflow.com/questions/216823/how-to-trim-a-stdstring
 	 */
 	void rtrim(std::string &s);
 	
 	/**
 	 * @brief trim from start and end
-	 * 
 	 * @param s string to trim
-	 * 
 	 * @see ns::ltrim
 	 * @see ns::rtrim
-	 * 
 	 * @note https://stackoverflow.com/questions/216823/how-to-trim-a-stdstring
 	 */
 	void trim(std::string& s);
