2946b4dd9a018bb252f9d8b8a7d720cd28129f07
SuadoCowboy <luccacsg@gmail.com>
2025-03-29 16:46:43 -0300

    make use of dllexport only for windows

--- src/DLLExport.h
+++ src/DLLExport.h
@@ -7,7 +7,7 @@
 		#define NIKIAPI
 	#endif
 #else
-	#if !defined(BUILD_SHARED) && !defined(BUILD_EXPORT)
+	#if !defined(BUILD_SHARED) && !defined(BUILD_EXPORT) && (defined(_WIN32) || defined(_WIN64))
 		#define NIKIAPI __declspec(dllexport)
 	#else
 		#define NIKIAPI
