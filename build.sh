1f459be0bae56f77ee35957110f2277be3b49e03
SuadoCowboy <luccacsg@gmail.com>
2025-04-18 14:17:38 -0300

    build became release32.sh and release64.sh

--- build.sh
+++
@@ -1,3 +0,0 @@
-cmake -Bbuild -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED=ON -DBUILD_64BITS=ON
-cd build
-make
---
+++ release32.sh
@@ -0,0 +1,2 @@
+cmake -Bbuild -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED=OFF -DBUILD_64BITS=OFF
+cmake --build build
