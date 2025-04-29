d4b5371e2a00508a637849d01a09953ed2c2c10c
SuadoCowboy <luccacsg@gmail.com>
2025-04-29 11:42:14 -0300

    removed unused param from docs

--- wrapper/BaseWrapper.h
+++ wrapper/BaseWrapper.h
@@ -9,7 +9,6 @@
 namespace ns {
 	/**
 	 * @brief should be used when a string is created in the function scope and not passed to the function as a parameter
-	 * @param size not counting null-terminator as it's added automatically
 	 */
 	char* allocStringToCharString(const std::string& src);
 
